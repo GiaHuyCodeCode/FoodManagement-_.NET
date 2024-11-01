aa6d1f2fc10594c10dc53d97f35ce261313b7997
Tran Gia Bao <tranb6778@gmail.com>
2024-11-02 06:37:17 +0700

    FE: add Product Web Form

---
+++ Admin/Product.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FoodShop.Admin
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        protected void btnAddOrUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void rProduct_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+
+        }
+
+        protected void rProduct_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+
+        }
+    }
+}
